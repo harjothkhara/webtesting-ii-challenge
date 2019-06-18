f3bf10968e3a6ef124d3a0868094c2d6221acf10
harjothkhara <harjoth.khara@gmail.com>
2019-06-18 16:01:35 -0700

    added methods to App for foulBall, ball, strike

--- baseball_testing/src/App.js
+++ baseball_testing/src/App.js
@@ -3,13 +3,40 @@ import Display from './components/Display'
 import Dashboard from './components/Dashboard'
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    strikes: 0,
+    balls: 0
+  };
+
+foulBall = () => {
+  this.setState(prevState => ({
+    strikes: prevState.strike < 2 ? prevState.strikes + 1 : prevState.strikes
+  }))
+}
+
+ball = () => {
+  this.setState(prevState => ({
+    balls: prevState.balls === 3 ? 0 : prevState.ball + 1,
+    strikes: prevState.balls === 3 ? 0 : prevState.strikes
+  }));
+};
+
+strike = () => {
+  this.setState(prevState => ({
+    balls: prevState.strikes === 2 ? 0 : prevState.balls, 
+    strikes: prevState.strikes === 2 ? 0 : prevState.strikes + 1 //strike out
+  }))
+};
+
+render() {
   return (
     <div className="App">
-     <Display />
-     <Dashboard />
+      <Display />
+      <Dashboard />
     </div>
   );
+ }
 }
 
 export default App;
