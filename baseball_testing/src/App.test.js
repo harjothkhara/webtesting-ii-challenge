709732309c7c5b8887fc48a3039b4d66384521ea
harjothkhara <harjoth.khara@gmail.com>
2019-06-18 18:02:23 -0700

    strike function tested

--- baseball_testing/src/App.test.js
+++ baseball_testing/src/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from "@testing-library/react"  // <<<install this
+// import "@testing-library/react/cleanup-after-each"
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +9,14 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it("renders successfully", () => {
+  render(<App />);
+});
+
+it('Successfully strikes', () => {
+  const { getAllByText, getByText } = render (<App />);
+  const button = getAllByText(/strike/i)[1];
+  fireEvent.click(button);
+  getByText(/1/);
+})
